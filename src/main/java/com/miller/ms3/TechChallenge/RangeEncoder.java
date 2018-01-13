00baa11043c04aa91c00d66dc1fb5fd0a534fda5
kmillerjr <kmillerjr@gmail.com>
2018-01-13 09:15:27 -0600

    implement reverse output and format java files

--- src/main/java/com/miller/ms3/TechChallenge/RangeEncoder.java
+++ src/main/java/com/miller/ms3/TechChallenge/RangeEncoder.java
@@ -13,19 +13,17 @@ public class RangeEncoder implements Callable {
 		// get the properties from the message
 		int startValue;
 		int endValue;
-		//TODO: boolean reversed;
+		boolean reversed;
 		RangeInterval[] encodedRange;
 		MuleMessage message = eventContext.getMessage();
-		
-		try {	
+
+		try {
 			startValue = Integer.parseInt(message.getInvocationProperty("startValue").toString());
 			endValue = Integer.parseInt(message.getInvocationProperty("endValue").toString());
-			// reversed =
-			// Boolean.parseBoolean(message.getInvocationProperty("reversed",
-			// "false"));
+			reversed = Boolean.parseBoolean(message.getInvocationProperty("reversed", "false"));
 
 			// encode range
-			encodedRange = Encode(startValue, endValue);
+			encodedRange = Encode(startValue, endValue, reversed);
 
 		} catch (Exception e) {
 			// log error and return empty range
@@ -35,11 +33,13 @@ public class RangeEncoder implements Callable {
 		return message;
 	}
 
-	private RangeInterval[] Encode(int startValue, int endValue) {
+	private RangeInterval[] Encode(int startValue, int endValue, boolean reversed) {
 
-		int incrValue = 1;
-		int arraySize = Math.abs(startValue- endValue)+1;
-		RangeInterval[]encodedRange = new RangeInterval[arraySize];
+		int incrValue = 1; // default to ascending range
+		int arrayDirection = 1; // default to forward, not reversed
+		int arrayIndex = 0; // default to starting at the beginning of the array
+		int arraySize = Math.abs(startValue - endValue) + 1;
+		RangeInterval[] encodedRange = new RangeInterval[arraySize];
 
 		// determine direction of iterator
 		if (startValue > endValue) {
@@ -47,19 +47,24 @@ public class RangeEncoder implements Callable {
 			incrValue = -1;
 		}
 
+		// determine the direction of the intervals
+		if (reversed) {
+			arrayDirection = -1;
+			arrayIndex = arraySize - 1; // start filling the array from the tail
+		}
+
 		// process first value in the range
 		int currentValue = startValue;
-		int arrayindex= 0;
-		encodedRange[arrayindex] = encodeValue(currentValue); 
+		encodedRange[arrayIndex] = encodeValue(currentValue);
 
 		// process remaining values in the range
 		while (currentValue != endValue) {
 			// move to next value in the range
 			currentValue = currentValue + incrValue;
-			arrayindex++;
-			
+			arrayIndex = arrayIndex + arrayDirection;
+
 			// encode current value
-			encodedRange[arrayindex] = encodeValue(currentValue);
+			encodedRange[arrayIndex] = encodeValue(currentValue);
 		}
 
 		return encodedRange;
@@ -69,7 +74,7 @@ public class RangeEncoder implements Callable {
 
 		RangeInterval rangeInterval = new RangeInterval();
 		String encodedValue;
-		
+
 		if (value % 3 == 0 && value % 7 == 0) {
 			// Multiples of both 7 and 3 convert to “MS3 and ME”
 			encodedValue = "MS3 and ME";
@@ -83,10 +88,10 @@ public class RangeEncoder implements Callable {
 			// encoded value is just the integer value
 			encodedValue = Integer.toString(value);
 		}
-		
+
 		rangeInterval.setValue(value);
 		rangeInterval.setEncoded(encodedValue);
-		
+
 		return rangeInterval;
 	}
 }
